93795ff09b9542edbc74fe245f47249cc0ab5ff6
MuhammadAzmiH2003 <MuhammadAzmiH2003>
2024-05-25 08:57:21 +0700

    first commit

---
+++ lib/core.dart
@@ -0,0 +1,10 @@
+/*
+We believe, the class name must be unique. 
+If there is a conflicting class name in this file,
+it means you have to rename it to something more unique.
+*/
+export 'package:ungebut/module/dashboard/controller/dashboard_controller.dart';
+export 'package:ungebut/module/dashboard/view/dashboard_view.dart';
+export 'package:ungebut/module/login/controller/login_controller.dart';
+export 'package:ungebut/module/login/view/login_view.dart';
+export 'package:ungebut/state_util.dart';
